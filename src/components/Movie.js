9ea2d3eecac6b29c95624354a4eecbe17541f1b1
Jenna Leopold <jleopold424@gmail.com>
2022-03-14 13:50:17 -0400

    created Movie component

---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Movie = (props) => {
+  return (
+    <div>
+      <h1>Lord of the Rings: {props.title}</h1>
+      <img src={props.poster} />
+      <p>
+        Runtime: {props.hours} hours and {props.minutes} minutes{' '}
+      </p>
+    </div>
+  )
+}
+
+export default Movie
