68873b1fadabe2a8eb37d0928341daa997fe53c1
Jenna Leopold <jleopold424@gmail.com>
2022-03-14 15:15:06 -0400

    refactored to use array of objects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import React from 'react'
 import './App.css'
 import Movie from './components/Movie'
 
+const movies = [
+  {
+    title: 'The Fellowship of the Ring',
+    hours: 2,
+    minutes: 58,
+    poster:
+      'https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg'
+  },
+  {
+    title: 'The Two Towers',
+    hours: 2,
+    minutes: 59,
+    poster:
+      'https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg'
+  },
+  {
+    title: 'The Return of the King',
+    hours: 3,
+    minutes: 21,
+    poster:
+      'https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'
+  }
+]
+
 const App = () => {
   return (
     <div className="App">
@@ -9,24 +33,15 @@ const App = () => {
         <h1>Lord of the Rings: A Trilogy</h1>
       </header>
       <main>
-        <Movie
-          title="The Fellowship of the Ring"
-          hours={2}
-          minutes={58}
-          poster="https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg"
-        />
-        <Movie
-          title="The Two Towers"
-          hours={2}
-          minutes={59}
-          poster="https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg"
-        />
-        <Movie
-          title="The Return of the King"
-          hours={3}
-          minutes={21}
-          poster="https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg"
-        />
+        {movies.map(({ title, hours, minutes, poster }) => (
+          <Movie
+            key={title}
+            title={title}
+            hours={hours}
+            minutes={minutes}
+            poster={poster}
+          />
+        ))}
       </main>
     </div>
   )
