87bcc8ca902af872caaf71d04a479486657de991
Jenna Leopold <jleopold424@gmail.com>
2022-03-14 13:51:21 -0400

    import & render 3 Movie components in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import './App.css'
+import Movie from './components/Movie'
 
 const App = () => {
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Lord of the Rings: A Trilogy</h1>
       </header>
       <main>
-
+        <Movie
+          title="The Fellowship of the Ring"
+          hours={2}
+          minutes={58}
+          poster="https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg"
+        />
+        <Movie
+          title="The Two Towers"
+          hours={2}
+          minutes={59}
+          poster="https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg"
+        />
+        <Movie
+          title="The Return of the King"
+          hours={3}
+          minutes={21}
+          poster="https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg"
+        />
       </main>
     </div>
   )
